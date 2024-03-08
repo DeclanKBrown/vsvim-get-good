50e83593ae7ef8df86933748609f3216ccd55d65
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2024-03-08 21:08:42 +1100

    hjkl game

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,96 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const boardSizeOptions: { [key: string]: number } = {
+    noob: 3,
+    easy: 5,
+    medium: 7,
+    hard: 8,
+    nightmare: 9,
+    tpope: 10
+};
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vsvim-get-good" is now active!');
+const instructions = [
+    "Remove the x with using only h,j,k,l, and x. Remember, this is for you to be better.",
+    "Using the arrow keys only makes you dumber. Trust me, I am a scientist",
+    "",
+];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vsvim-get-good.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vsvim-get-good!');
-	});
+class HjklRound {
+    private window: vscode.TextEditor;
+    private difficulty: string;
 
-	context.subscriptions.push(disposable);
+    constructor(difficulty: string, window: vscode.TextEditor) {
+        this.window = window;
+        this.difficulty = difficulty;
+    }
+
+    getInstructions(): string[] {
+        return instructions;
+    }
+
+    getRandomNumber(count: number): number {
+        return Math.floor(Math.random() * count) + 1;
+    }
+
+    checkForWin(): boolean {
+        const lines = this.window.document.getText().split('\n');
+        const found = lines.some(line => line.includes('x'));
+        return !found;
+    }
+
+    render() {
+        const boardSize = boardSizeOptions[this.difficulty];
+
+        let xLine = 0, xCol = 0, cursorLine = 0, cursorCol = 0;
+        do {
+            xCol = this.getRandomNumber(boardSize);
+            xLine = this.getRandomNumber(boardSize);
+            cursorCol = this.getRandomNumber(boardSize);
+            cursorLine = this.getRandomNumber(boardSize);
+        } while (xLine === cursorLine || xCol === cursorCol);
+
+        const lines = new Array(boardSize).fill('').map(() => new Array(boardSize).fill(' ').join(''));
+        lines[xLine - 1] = lines[xLine - 1].slice(0, xCol - 1) + 'x' + lines[xLine - 1].slice(xCol);
+
+        this.window.edit(editBuilder => {
+            editBuilder.replace(new vscode.Range(0, 0, lines.length, 0), lines.join('\n'));
+        });
+
+        this.window.selection = new vscode.Selection(new vscode.Position(cursorLine - 1, cursorCol - 1), new vscode.Position(cursorLine - 1, cursorCol - 1));
+        this.window.revealRange(new vscode.Range(new vscode.Position(cursorLine - 1, cursorCol - 1), new vscode.Position(cursorLine - 1, cursorCol - 1)));
+    }
+
+    name(): string {
+        return "hjkl";
+    }
+
+    start() {
+        this.render();
+
+        const handleWin = () => {
+            if (this.checkForWin()) {
+                this.render();
+            }
+        };
+
+        const disposable = vscode.workspace.onDidChangeTextDocument(event => {
+            if (event.document === this.window.document) {
+                handleWin();
+            }
+        });
+    }
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+// Usage example:
+function activate(context: vscode.ExtensionContext) {
+    const disposable = vscode.commands.registerCommand('vsvim-get-good.start', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const round = new HjklRound('easy', editor);
+            round.start();
+        }
+    });
+    context.subscriptions.push(disposable);
+}
+
+export { activate };
